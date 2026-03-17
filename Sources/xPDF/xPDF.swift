f8959c822f041a5dead8307ef8177929885f2b4b
d14ceb37e82ddfe68777e8454997ed7d <216149341+d14ceb37e82ddfe68777e8454997ed7d@users.noreply.github.com>
2026-03-18 00:40:56 +0800

    修复

--- Sources/xPDF/xPDF.swift
+++ Sources/xPDF/xPDF.swift
@@ -62,6 +62,7 @@ struct ContentView: View {
                         )
                     }
                     NSWorkspace.shared.open(directoryURL)
+                    self.document = nil
                 }
             }
         }
